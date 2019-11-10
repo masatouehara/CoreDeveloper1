56178555cf5f6c1feef7cdad69f63d46658b40b2
masatouehara <u.masato10@gmail.com>
2019-11-10 17:40:28 +0900

    exercise.py

--- week4/exercise.py
+++ week4/exercise.py
@@ -7,12 +7,17 @@
 # The function below is an implementation of bubble sort
 # Rewrite it with proper comments and formatting using Clean Code standards
 
+#Function: Bubble sort
+
 def bs(x):
     n = len(x)
-    for i in range(n):
+    for i in range(n): # Search all number
         for j in range(0, n-i-1):
             if x[j] > x[j+1] :
                 x[j], x[j+1] = x[j+1], x[j]
+
+
+
 
 # EXERCISE 2:
 
@@ -22,3 +27,17 @@ def bs(x):
 # Write a proper multiline docstring for your code
 
 # - - - - Enter code here or in separate file - - - -
+
+def Substring(word,substring):
+    n = len(word)
+    count = 0
+    for i in range(n):
+        if word[i] == substring:
+            count += 1
+    return count
+
+word = "abdsa"
+substring = "b"
+print(Substring(word,substring))
+
+    
